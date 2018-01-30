19f690c3bb820b6f639340bb00225f368c687195
ambid17 <sbambrose@unomaha.edu>
2018-01-29 23:29:20 -0600

    Added 2 basic hovercars
    the hovercars are buggy but they work

---
+++ ProjectHover/Assets/Scripts/HoverAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverAudio : MonoBehaviour {
+
+    public AudioSource jetSound;
+    private float jetPitch;
+    private const float LowPitch = .1f;
+    private const float HighPitch = 2.0f;
+    private const float SpeedToRevs = .01f;
+    Vector3 myVelocity;
+    Rigidbody carRigidBody;
+
+    private void Awake()
+    {
+        carRigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        myVelocity = carRigidBody.velocity;
+        float forwardSpeed = transform.InverseTransformDirection(carRigidBody.velocity).z;
+        float engineRevs = Mathf.Abs(forwardSpeed) * SpeedToRevs;
+        jetSound.pitch = Mathf.Clamp(engineRevs, LowPitch, HighPitch);
+	}
+}
---
+++ ProjectHover/Assets/Scripts/HoverThrusterController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverThrusterController : MonoBehaviour {
+    public float acceleration;
+    public float rotationRate;
+
+    public float turnRotationAngle;
+    public float turnRotationSeekSpeed;
+
+    //Reference Variables
+    private float rotationVelocity;
+    private float groundAngleVelocity;
+
+    Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        if(Physics.Raycast(transform.position, -transform.up, 3f))
+        {
+            rigidbody.drag = 1;
+
+            Vector3 forwardForce = transform.forward * acceleration * Input.GetAxis("Vertical");
+
+            forwardForce = forwardForce * Time.deltaTime * rigidbody.mass;
+
+            rigidbody.AddForce(forwardForce);
+        }
+        else
+        {
+            rigidbody.drag = 0;
+        }
+
+        Vector3 turnTorque = Vector3.up * rotationRate * Input.GetAxis("Horizontal");
+
+        turnTorque = turnTorque * Time.deltaTime * rigidbody.mass;
+        rigidbody.AddTorque(turnTorque);
+
+
+        //fakes the car rotation when strafing
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z = Mathf.SmoothDampAngle(newRotation.z, Input.GetAxis("Horizontal") * -turnRotationAngle, ref rotationVelocity, turnRotationSeekSpeed);
+        transform.eulerAngles = newRotation;
+    }
+}
