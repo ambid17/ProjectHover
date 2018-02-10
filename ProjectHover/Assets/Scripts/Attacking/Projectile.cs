a6874cb3c21d368662b1a7819b5a3e56f3ddf220
max_schuessler <mschuessler@unomaha.edu>
2018-02-10 16:19:30 -0600

    Created base weapon scripts and weapon swapping. Also imported standard assets particle effects

---
+++ ProjectHover/Assets/Scripts/Attacking/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	[SerializeField]
+	private float speed, damage;
+
+	[SerializeField]
+	private bool explosive;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+
+	private void OnCollisionEnter(Collision c) {
+		if (explosive) {
+
+		}
+	}
+}
---
+++ ProjectHover/Assets/Scripts/WeaponProperties.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponProperties : MonoBehaviour {
+
+	private string wepName, type;
+	private float damagePerTick, fireRate, ammoMax, ammoCurr, reloadSpeed;
+	private bool ammo, recoi;
+	
+
+}
